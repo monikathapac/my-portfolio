3d7a6ca69e17849e83e2e9cce03c66c56a3556bc
monika <monikathapa007@gmail.com>
2025-07-12 12:57:25 +0530

    initial

--- src/Footer.js
+++ src/Footer.js
@@ -1,44 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons';
-import './FooterSection.css';
-function Footer(){
-    return(
-      <div div id="footer">
-        <div className="footer-container" >
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as fasHeart } from "@fortawesome/free-solid-svg-icons";
+import "./FooterSection.css";
+function Footer() {
+  return (
+    <div div id="footer">
+      <div className="footer-container">
+        <div className="footer-content">
+          <h3>Monika's portfolio</h3>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#">Service</a>
+            </li>
+            <li>
+              <a href="#project">Projects</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="#">Rewiew</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+            <li>
+              <a href="#contact">Contact me</a>
+            </li>
 
-     <div className="footer-content"  >     
-  <h3>Monika's portfolio</h3>
-    <ul>
-      <li>
-      <Link to="/home">Home</Link></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#">Service</a></li>
-      <li><a href="#project">Projects</a></li>
-     
-      </ul><ul>
-      <li><a href="#">Rewiew</a></li>
-      <li><a href="#">Support</a></li>
-      <li><a href="#contact">Contact me</a></li>
-      
-       <li><Link to="/faq">FAQ</Link></li>
-
-  
-    </ul></div>
-
-   
-  
-  
-  </div> 
-     <div class="footer-text">Made with <FontAwesomeIcon icon={ fasHeart} style={{color: "#f03838",}} /> by Monika Thapa</div>
-  
-
-
-</div>
-        
-    );
-
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="footer-text">
+        Made with{" "}
+        <FontAwesomeIcon icon={fasHeart} style={{ color: "#f03838" }} /> by
+        Monika Thapa
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- src/ProjectSection.js
+++ src/ProjectSection.js
@@ -1,49 +1,57 @@
 import React from "react";
 import "./ProjectSection.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressCard,
+  faCartShopping,
+  faClock,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
 
 function ProjectSection() {
   return (
-   
     <div id="project">
-     
       <div className="project-container">
         <div className="project-content">
           <h1>My projects</h1>
           <div className="cards">
             <div className="card">
               <h2>Shopping App</h2>
-              <img src="shop.jpeg" alt="insert image"></img>
+              <FontAwesomeIcon icon={faCartShopping} className="my_icon" />
               <p>
-                Created a full stack Shopppping app using Spring Boot &
-                react.js.
+                Created a full stack Shopping app using Spring Boot, with
+                features like auction & filters.
               </p>
               <a href="#">
                 <span className="more">Github link</span>
               </a>
             </div>
             <div className="card">
-              <h2>My portfolio</h2>
-              <img src="email.jpeg" alt="insert image"></img>
+              <h2>Weather app</h2>
+              <FontAwesomeIcon icon={faSun} className="my_icon" />
               <p>
-                This portfolio that you seeing is one of my first one.
+                Weather app using Javascript APIs on top of HTML/CSS. Gives you
+                real time weather, temperature & overall climate of that place.{" "}
               </p>
+              <a href="https://weather-app-i226.vercel.app" target="_blank">
+                <span className="more">check now</span>
+              </a>
+            </div>
+            <div className="card">
+              <h2>My portfolio</h2>
+
+              <FontAwesomeIcon icon={faAddressCard} className="my_icon" />
+              <p>This portfolio that you seeing is one of my first one.</p>
               <a href="#home">
                 <span className="more">Click</span>
               </a>
             </div>
+
             <div className="card">
-              <h2>Weather app</h2>
-              <img src="we.jpg" alt="insert image"></img>
-              <p>Weather app using API </p>
-              <a href="#" target="_blank">
-                <span className="more">check here</span>
-              </a>
-            </div>
-            <div className="card">
-              <h2>Basics</h2>
-              <img src="basic.jpeg" alt="insert image"></img>
-              <p>Created To-do app, clock, weather forcast app etc </p>
+              <h2>Timer watch</h2>
+
+              <FontAwesomeIcon icon={faClock} className="my_icon" />
+              <p>Real time watch with timer, calculator etc</p>
               <a href="https://timer-app-ashy.vercel.app" target="_blank">
                 <span className="more">check now</span>
               </a>
